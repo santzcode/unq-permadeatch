17abd92c334c5771408b60d46d84efa393a6461c
Santz Development <snowdicsdorolax@gmail.com>
2026-03-03 10:25:26 -0300

    Update kick message and add Discord webhook config
    Added custom kick message and Discord webhook configuration.

--- config.lua
+++ config.lua
@@ -6,8 +6,14 @@ Config.Framework = "creative"
 -- Arma que ativa o sistema
 Config.Weapon = "WEAPON_DOUBLEACTION"
 
--- Mensagem personalizada ao ser kickado
+-- Mensagem personalizada
 Config.KickMessage = "Sua história chegou ao fim..."
 
--- Tempo antes do kick (ms)
+-- Delay antes do kick (ms)
 Config.DelayBeforeKick = 4000
+
+-- WEBHOOK DISCORD
+Config.DiscordWebhook = "COLE_SEU_WEBHOOK_AQUI"
+
+Config.LogTitle = "💀 MORTE PERMANENTE"
+Config.LogColor = 16711680 -- vermelho

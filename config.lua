d17e1275b05afc11ee31a4f27e43b7b7eafd6191
Santz Development <snowdicsdorolax@gmail.com>
2026-03-03 10:20:32 -0300

    Add configuration settings for the creative framework

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- FRAMEWORK: "esx", "qbcore", "qbox", "creative"
+Config.Framework = "creative"
+
+-- Arma que ativa o sistema
+Config.Weapon = "WEAPON_DOUBLEACTION"
+
+-- Mensagem personalizada ao ser kickado
+Config.KickMessage = "Sua história chegou ao fim..."
+
+-- Tempo antes do kick (ms)
+Config.DelayBeforeKick = 4000

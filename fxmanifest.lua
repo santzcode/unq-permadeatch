c8ebd7bfe8993190ab071ed2e65111b2aa1d394c
Santz Development <snowdicsdorolax@gmail.com>
2026-03-03 10:19:44 -0300

    Create fxmanifest.lua for resource setup
    Add fxmanifest for Golden Double Action Kick System

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Vitor Santos'
+description 'Golden Double Action - Kick System'
+version '1.1.0'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
